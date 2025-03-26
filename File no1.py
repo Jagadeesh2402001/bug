8c7c4da091d86afdeb5f0c23ccd5afaa84b6ae5e
Jagadeesh2402001 <jagadeesh.v@haystackrobotics.com>
2025-03-26 12:53:15 +0530

    Update File no1.py
    Local variables

--- File no1.py
+++ File no1.py
@@ -1,8 +1,15 @@
 # 1st Code: Robot Moves 5 Meters
+
+DEBUG = False
+if DEBUG:
+    IMAGE_PATH = "/home/hariharan/"
+    DISINFECT_NAVIGATION-LAUNCH_PATH = ["/home/hariharan/turtlebot/src/exploration/launch/turtlebot_nav.launch"]
+
 class Robot:
     def move_forward(self, distance):
         # Simulate robot moving forward by distance
         print(f"Robot is moving forward {distance} meters.")
+        
 
 # Create robot object
 robot = Robot()
